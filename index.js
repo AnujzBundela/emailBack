45cea32d3173375690df69d1701bd472c0dca7ee
AnujzBundela <anujbundela17112001@gmail.com>
2025-03-15 13:41:28 +0530

    terts

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
-app.use(express.json()); 
-app.use(cors()); 
+app.use(express.json());
+app.use(cors());
 // { origin: " https://anujzbundela.github.io/Submition/" }
 
 app.use("/api", contactRoutes);
